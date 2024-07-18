0ea6e90c0a6b0d797f652d6faa6a11007c9e274e
Sebastian <csbastian88@gmail.com>
2024-07-18 19:05:24 -0300

    Inicio de modificacion usando update

--- routes/api.php
+++ routes/api.php
@@ -10,22 +10,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/personas', function(){
-    return 'Obteniendo Lista de Personas';
-});
-
-Route::get('/personas/{id}' , function(){
-    return 'Obteniendo una persona';
-});
-
-Route::post('/personas', function(){
-    return 'Creando Personas';
-});
 
 Route::put('/personas/{id}' , function(){
     return 'Modificando Persona';
 });
 
-Route::delete('/personas/{id}' , function(){
-    return 'Eliminando Persona';
-});
+
