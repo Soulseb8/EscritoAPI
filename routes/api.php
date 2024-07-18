07ccaf7299f62231751147913e8527cab177bc04
Sebastian <csbastian88@gmail.com>
2024-07-18 17:02:50 -0300

    Agregamos los campos de nombre apellido y telefono en la ruta de migrations

--- routes/api.php
+++ routes/api.php
@@ -11,21 +11,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/personas', function(){
-    return "Obteniendo Lista de Personas";
+    return 'Obteniendo Lista de Personas';
 });
 
-Route::get('/personas/{id}', function(){
-    return "Obteniendo una persona";
+Route::get('/personas/{id}' , function(){
+    return 'Obteniendo una persona';
 });
 
 Route::post('/personas', function(){
-    return "Creando Personas";
+    return 'Creando Personas';
 });
 
-Route::put('/personas/{id}', function(){
-    return "Modificando Persona";
+Route::put('/personas/{id}' , function(){
+    return 'Modificando Persona';
 });
 
-Route::delete('/personas/{id}', function(){
-    return "Eliminando Persona";
+Route::delete('/personas/{id}' , function(){
+    return 'Eliminando Persona';
 });
