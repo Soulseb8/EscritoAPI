44728a39ed89f48a098f7827fd8360f0ef5bdaf6
Sebastian <csbastian88@gmail.com>
2024-07-18 15:27:55 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonaController;
+
+Route::apiResource('personas', PersonaController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
