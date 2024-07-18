377d58d84817db0e09ef63516e6b27668426f8b1
Sebastian <csbastian88@gmail.com>
2024-07-18 16:45:12 -0300

    añadi 4 rutas para hacia persona en la api.php

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,19 @@ Route::apiResource('personas', PersonaController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/personas', function(){
+    return "Lista de Personas";
+});
+
+Route::post('/personas', function(){
+    return "Creando Personas";
+});
+
+Route::put('/personas/{id}', function(){
+    return "Modificando Persona";
+});
+
+Route::delete('/personas/{id}', function(){
+    return "Eliminando Persona";
+});
